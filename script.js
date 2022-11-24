56a7a54d899c877fd43e6a509f6a8e79b2ab4287
Aarnav Gill <gill.aarnav@gmail.com>
2022-11-23 23:12:03 -0800

    button name changes to lightmode

--- script.js
+++ script.js
@@ -4,5 +4,22 @@ const Darkmode = document.querySelector(".Darkmode");
 function darkmode(){
     //  document.body.style.backgroundColor = "black";
     document.body.classList.add("black-bg")
+    
 }
 Darkmode.addEventListener("click",darkmode)
+
+
+function changetext(){
+    if(Darkmode.textContent==="Dark mode")
+    {
+        Darkmode.textContent="Lightmode";
+    }
+    else if(Darkmode.textContent==="Lightmode")
+    {
+       
+        Darkmode.textContent="Dark mode";
+    }
+    
+
+}
+Darkmode.addEventListener("click",changetext);
