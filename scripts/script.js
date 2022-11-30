bf8c3d26f6aa16bf142f9d17c4cf7a610e29852b
Aarnav Gill <gill.aarnav@gmail.com>
2022-11-30 14:54:03 -0800

    darkmode function is working properly and everything including text change and reverting back to light mode is also working

--- scripts/script.js
+++ scripts/script.js
@@ -6,24 +6,44 @@ const buttonContainer = document.querySelector(".firstbuttons");
 const notepad= document.querySelector("#notepad");
 const sidenav = document.querySelector(".sidebar");
 
-function darkmode(){
+function darkmodeforbackg(){
     //  document.body.style.backgroundColor = "black";
-    document.body.classList.add("black-bg") 
-}
-Darkmode.addEventListener("click",darkmode)
+    document.body.classList.toggle("black-bg") 
 
+    
+}
+Darkmode.addEventListener("click",darkmodeforbackg)
 
 function changetext(){
-    if(Darkmode.textContent==="Dark mode")
+    if(Darkmode.textContent==="Dark Mode")
     {
-        Darkmode.textContent="Lightmode";
+        Darkmode.textContent="Light Mode";
     }
-    else if(Darkmode.textContent==="Lightmode")
+    else if(Darkmode.textContent==="Light Mode")
     {
-       
-        Darkmode.textContent="Dark mode";
+        Darkmode.textContent="Dark Mode";
     }
-    
-
 }
 Darkmode.addEventListener("click",changetext);
+
+function changetextandchangecolorsofelements(buttontext){
+    if( buttontext.target.textContent==="Light Mode"){
+        save.style.backgroundColor="black"
+        newnotebutton.style.backgroundColor="black"
+        cancelbutton.style.backgroundColor="black"
+        sidenav.style.backgroundColor="#252525"
+        notepad.style.backgroundColor="#696969"
+        buttontext.target.style.backgroundColor="black"
+       }
+
+       if( buttontext.target.textContent==="Dark Mode"){
+        save.style.backgroundColor="#55869b"
+        newnotebutton.style.backgroundColor="#55869b"
+        cancelbutton.style.backgroundColor="#E9806E"
+        sidenav.style.backgroundColor="whitesmoke"
+        notepad.style.backgroundColor="whitesmoke"
+        buttontext.target.style.backgroundColor="#685369"
+        }
+
+}
+buttonContainer.addEventListener("click",changetextandchangecolorsofelements);
